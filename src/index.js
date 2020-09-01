579ad0144f37a14aaea10b439e07455d4c77de90
Katie Poa <kpoa@berkeley.edu>
2020-08-31 17:13:31 -0700

    Render App to DOM, implement VideoListEntry and VideoList and reference it in App.js make sure to import each Class we are referring to between files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// babel --watch . --out-dir compiled --presets=react --ignore=node_modules,compiled --source-maps inline
+
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
 
